55283b6c9ce9c03cc9474dd36fde05c1aa4ca778
NaelSkrrrt <ralalanael@gmail.com>
2024-04-17 16:01:10 +0300

    [IMPROVE] tabledata
    read, create

--- mynext-prisma-mysql/components/tabledata.tsx
+++ mynext-prisma-mysql/components/tabledata.tsx
@@ -1,35 +1,33 @@
+import { getEmployeeList } from "@/lib/action";
 
-const Employee = async () => {
+const Employee = async ({ query }: { query: string }) => {
+  const employees = await getEmployeeList(query);
   return (
     <table className="table table-zebra">
-        <thead className="text-sm text-gray-700 uppercase bg-gray-50">
-            <tr>
-                <th className="py-3 px-6">#</th>
-                <th className="py-3 px-6">Name</th>
-                <th className="py-3 px-6">Email</th>
-                <th className="py-3 px-6">Phone Number</th>
-                <th className="py-3 px-6">Created At</th>
-                <th className="py-3 px-6 text-center">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr className="bg-white border-b text-slate-800">
-                <td className="py-3 px-6">1</td>
-                <td className="py-3 px-6">Blabla coder </td>
-                <td className="py-3 px-6">blabla@exemple.com</td>
-                <td className="py-3 px-6">+261 32 44 405 60</td>
-                <td className="py-3 px-6">
-                    July 11
-                </td>
-                <td className="justify-center flex gap-1 py-3">
-                    Edit | Delete
-                </td>
-
-            </tr>
-        </tbody>
-
+      <thead className="text-sm text-gray-700 uppercase bg-gray-50">
+        <tr>
+          <th className="py-3 px-6">#</th>
+          <th className="py-3 px-6">Name</th>
+          <th className="py-3 px-6">Email</th>
+          <th className="py-3 px-6">Phone Number</th>
+          <th className="py-3 px-6">Created At</th>
+          <th className="py-3 px-6 text-center">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {employees.map((rs, index) => (
+          <tr key={rs.id} className="bg-white border-b text-slate-800">
+            <td className="py-3 px-6">{index + 1}</td>
+            <td className="py-3 px-6">{rs.name}</td>
+            <td className="py-3 px-6">{rs.email}</td>
+            <td className="py-3 px-6">{rs.phone}</td>
+            <td className="py-3 px-6">July 11</td>
+            <td className="justify-center flex gap-1 py-3">Edit | Delete</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
-  )
-}
+  );
+};
 
-export default Employee
+export default Employee;
